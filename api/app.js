275d182769cc82ad9e10bd5fbdc749bcc05f994a
Ruslana Logosha <ruslanalogosha@gmail.com>
2021-03-18 22:23:36 +0300

    tries to deploy back and front

--- api/app.js
+++ api/app.js
@@ -20,6 +20,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
+app.get('/', (req, res) => {
+  res.send('Hello from Express!');
+});
+
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' });
 });
